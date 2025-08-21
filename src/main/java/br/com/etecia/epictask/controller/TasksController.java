44eb3d8183c33404eb5e77a041acb3379d9ab331
Nayra-pixel <nayrasduarte@gmail.com>
2025-08-20 22:14:32 -0300

    second commit

--- src/main/java/br/com/etecia/epictask/controller/TasksController.java
+++ src/main/java/br/com/etecia/epictask/controller/TasksController.java
@@ -1,8 +1,12 @@
 package br.com.etecia.epictask.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.etecia.epictask.model.Task;
 
 @Controller
 @RequestMapping("/tasks")
@@ -11,8 +15,14 @@ public class TasksController {
     @GetMapping
 
     public String listTasks(){
-
-        System.out.println("Listar Tarefas");
+        
+        
+        var tasks = List.of(
+            new Task(1L, "Castração", "Castração Pitoco", 50, 0), 
+            new Task(1L, "Vacinação", "Vacinação Pitoco", 50, 0), 
+            new Task(1L, "Exame de Sangue", "Exame Pitoco", 50, 0)
+            );
+        
         return "tasks";
 
     }
