886aab79b659deff60908695e1344abe674edb84
Nayra-pixel <nayrasduarte@gmail.com>
2025-08-27 22:20:53 -0300

    form

--- src/main/java/br/com/etecia/epictask/controller/TasksController.java
+++ src/main/java/br/com/etecia/epictask/controller/TasksController.java
@@ -1,10 +1,13 @@
 package br.com.etecia.epictask.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.etecia.epictask.model.Task;
@@ -13,21 +16,29 @@ import br.com.etecia.epictask.model.Task;
 @RequestMapping("/tasks")
 public class TasksController {
     
+    private List<Task> repository= new ArrayList<>();
+
     @GetMapping
 
     public String listTasks(Model model){
         
-        
-        var tasks = List.of(
-            new Task(1L, "Castração", "Castração Pitoco", 50, 0), 
-            new Task(1L, "Vacinação", "Vacinação Pitoco", 50, 0), 
-            new Task(1L, "Exame de Sangue", "Exame Pitoco", 50, 0)
-            );
-        
-            model.addAttribute("tasks", tasks);
+            model.addAttribute("tasks", repository);
 
         return "tasks";
 
+    }
+
+    @GetMapping("/form")
+    public String showForm(){
+        return "form";
+
+    }
+
+    @PostMapping("/form")
+    public String create(Task task){
+        System.out.println("Cadastrando Procedimento..." + task);
+        repository.add(task);
+        return "form";
     }
 
 }
