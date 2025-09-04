cffd5f1d5549381f3c8f94dab5dfebf44e434ff6
Nayra-pixel <nayrasduarte@gmail.com>
2025-09-03 22:18:41 -0300

    database h2 jpa

--- src/main/java/br/com/etecia/epictask/controller/TasksController.java
+++ src/main/java/br/com/etecia/epictask/controller/TasksController.java
@@ -3,6 +3,7 @@ package br.com.etecia.epictask.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,33 +13,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.etecia.epictask.model.Task;
+import br.com.etecia.epictask.repository.TaskRepository;
 
 @Controller
 @RequestMapping("/tasks")
 public class TasksController {
     
-    private List<Task> repository= new ArrayList<>();
+    //private List<Task> repository= new ArrayList<>();
+
+    @Autowired
+    private TaskRepository repository;
 
     @GetMapping
-
     public String listTasks(Model model){
-        
-            model.addAttribute("tasks", repository);
-
+        model.addAttribute("tasks", repository.findAll());
         return "tasks";
-
     }
 
     @GetMapping("/form")
     public String showForm(){
         return "form";
-
     }
 
     @PostMapping("/form")
     public String create(Task task, RedirectAttributes redirect){
         System.out.println("Cadastrando Procedimento..." + task);
-        repository.add(task);
+        repository.save(task);
         redirect.addFlashAttribute("message", "Procedimento Cadastrado com Sucesso");
         return "redirect:/tasks";
     }
