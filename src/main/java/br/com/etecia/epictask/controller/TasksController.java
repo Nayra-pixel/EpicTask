b8c79f9f780f893e5eb6a0f1051d4ed955941ba1
Nayra-pixel <nayrasduarte@gmail.com>
2025-09-10 22:18:47 -0300

    validation

--- src/main/java/br/com/etecia/epictask/controller/TasksController.java
+++ src/main/java/br/com/etecia/epictask/controller/TasksController.java
@@ -1,11 +1,9 @@
 package br.com.etecia.epictask.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -14,6 +12,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.etecia.epictask.model.Task;
 import br.com.etecia.epictask.repository.TaskRepository;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/tasks")
@@ -31,12 +30,13 @@ public class TasksController {
     }
 
     @GetMapping("/form")
-    public String showForm(){
+    public String showForm(Task task){
         return "form";
     }
 
     @PostMapping("/form")
-    public String create(Task task, RedirectAttributes redirect){
+    public String create(@Valid Task task, BindingResult result, RedirectAttributes redirect){
+        if(result.hasErrors())return "form";
         System.out.println("Cadastrando Procedimento..." + task);
         repository.save(task);
         redirect.addFlashAttribute("message", "Procedimento Cadastrado com Sucesso");
