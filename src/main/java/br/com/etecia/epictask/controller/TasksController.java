095cefa07e8e54b73bf558ac1c19130a3e18dd33
Nayra-pixel <nayrasduarte@gmail.com>
2025-12-10 16:13:53 -0300

    Revert "login e logout"
    This reverts commit 46a6c68d0c35c845382b498127ee41a083dc5ed2.

--- src/main/java/br/com/etecia/epictask/controller/TasksController.java
+++ src/main/java/br/com/etecia/epictask/controller/TasksController.java
@@ -1,13 +1,13 @@
 package br.com.etecia.epictask.controller;
 
-import br.com.etecia.epictask.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.etecia.epictask.model.Task;
@@ -23,16 +23,10 @@ public class TasksController {
     @Autowired
     private TaskRepository repository;
 
-    @Autowired
-    private UserService userService;
-
     @GetMapping
-    public String listTasks(Model model, @AuthenticationPrincipal OAuth2User user) {
-         model.addAttribute("tasks", repository.findByStatusLessThan(100));
-        model.addAttribute("user", user);
-    
+    public String listTasks(Model model){
+        model.addAttribute("tasks", repository.findAll());
         return "tasks";
-    
     }
 
     @GetMapping("/form")
@@ -46,31 +40,6 @@ public class TasksController {
         System.out.println("Cadastrando Procedimento..." + task);
         repository.save(task);
         redirect.addFlashAttribute("message", "Procedimento Cadastrado com Sucesso");
-        return "redirect:/tasks";
-    }
-
-     @DeleteMapping("/{id}")
-    public String delete(@PathVariable Long id, RedirectAttributes redirect ){
-        System.out.println("Deletando tarefa " + id);
-        repository.deleteById(id);
-        redirect.addFlashAttribute("message", "Tarefa deletada");
-        return "redirect:/tasks";
-    }
-
-     @PutMapping("{id}/catch")
-    public String catchTask(@PathVariable Long id, @AuthenticationPrincipal OAuth2User oauth2User){
-        var task = repository.findById(id).get();
-        var user = userService.register(oauth2User);
-        task.setUser(user);
-        repository.save(task);
-        return "redirect:/tasks";
-    }
-
-    @PutMapping("{id}/add")
-    public String addStatus(@PathVariable Long id){
-        var task = repository.findById(id).get();
-        task.setStatus(task.getStatus() + 10);
-        repository.save(task);
         return "redirect:/tasks";
     }
 
--- src/main/java/br/com/etecia/epictask/repository/TaskRepository.java
+++ src/main/java/br/com/etecia/epictask/repository/TaskRepository.java
@@ -4,10 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.etecia.epictask.model.Task;
 
-import java.util.List;
-
 public interface TaskRepository  extends JpaRepository<Task, Long>{
     
-    List<Task> findByStatusLessThan(int status);
-
 }
